4d9b81efaeaf63fe488abcba81f43d1580e05b97
kev-cisra <87038939+kev-cisra@users.noreply.github.com>
2024-07-11 11:34:27 -0600

    Add files via upload

---
+++ database/migrations/2024_07_11_155955_create_personals_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('personals', function (Blueprint $table) {
+            $table->id();
+
+            $table->string("nombre", 80);
+            $table->string("ApPat", 80);
+            $table->string("ApMat", 80);
+            $table->string("clavePais", 10);
+            $table->integer("Telefono");
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('personals');
+    }
+};
