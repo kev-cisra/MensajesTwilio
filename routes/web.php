3d89c93ffe9a0c930f5d7d048bc8f4ef35cbf2a6
kev-cisra <87038939+kev-cisra@users.noreply.github.com>
2024-07-11 11:35:55 -0600

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('Welcome'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+});
+
+Route::prefix("SendMesa")
+->middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])
+->group(function(){
+    Route::post("/", function(Request $request){
+        $phone = $request->claPais.$request->telefono;
+        // return $phone;
+        return mensa_twilio($phone, $request->mensaje);
+    });
+});
